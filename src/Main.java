26639daad5b37277a024e9885ce5c663a446ea2c
tobygoetz <goetz.tobiah@student.greenriver.edu>
2023-06-06 21:47:49 -0700

    Adding our initial files, just a hellow world example.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * An example program
+ *
+ * @author tobygoetz
+ * @version 1.0
+ */
+
 public class Main
 {
+    /**
+     * Entry point for the application
+     * @param args command line args
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
